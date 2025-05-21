04f1da58af9a9dee63277ff48e98bc2e3e8d93f9
oltikamberi <okamberi32@gmail.com>
2025-05-21 19:41:06 +0200

    Pjesa e lidhjes me databazen

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,20 @@
-// PWD 1a51Z00KdB9JEec5
+import express, { Express } from "express";
+import mongoose from "mongoose";
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+//Lidhja me databaze
+const mongoURI: string =
+  "mongodb+srv://okamberi32:1a51Z00KdB9JEec5@personalfinance-cluster.9bapgxw.mongodb.net/";
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("CONNECTED TO MONGODB"))
+  .catch((err) => console.error("Failed to Connect to MongoDB", err));
+
+app.listen(port, () => {
+  console.log(`Server running on portin ${port}`);
+});
