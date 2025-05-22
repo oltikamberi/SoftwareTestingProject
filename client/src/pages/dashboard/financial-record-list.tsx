ab87336928807ce966481ed36ed0242918a17822
oltikamberi <okamberi32@gmail.com>
2025-05-22 17:01:40 +0200

    Shtimi i tabeles se rekordave

--- client/src/pages/dashboard/financial-record-list.tsx
+++ client/src/pages/dashboard/financial-record-list.tsx
@@ -1,3 +1,42 @@
+import { cloneElement } from 'react';
+import { useFinancialRecords } from '../../contexts/financial-record-context';
+import { useTable, type Column, type CellProps, type Row } from 'react-table';
+
 export const FinancialRecordList = () => {
-  return <div> Record list </div>;
+  const { records } = useFinancialRecords();
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data: records,
+    });
+
+  return (
+    <div className="table-container">
+      <table {...getTableProps()} className="table">
+        <thead>
+          {headerGroups.map((hg) => (
+            <tr {...hg.getHeaderGroupProps()}>
+              {hg.headers.map((column) => (
+                <th {...column.getHeaderProps()}> {column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, idx) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}> {cell.render('Cell')} </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
