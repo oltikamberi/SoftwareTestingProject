c38f4156ac7e62ec184b8eb8aade37a93d0348ef
alketagg <alketagashii13@gmail.com>
2025-05-22 14:47:58 +0200

    Routes te shtuara per financial records

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-records";
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
@@ -14,6 +15,8 @@ mongoose
   .connect(mongoURI)
   .then(() => console.log("CONNECTED TO MONGODB"))
   .catch((err) => console.error("Failed to Connect to MongoDB", err));
+
+app.use("/financial-records", financialRecordRouter);
 
 app.listen(port, () => {
   console.log(`Server running on portin ${port}`);
--- server/src/routes/financial-records.ts
+++ server/src/routes/financial-records.ts
@@ -2,3 +2,39 @@ import express, { Request, Response } from "express";
 import FinancialRecordModel from "../schema/financial-record";
 
 const router = express.Router();
+
+//nese daj error munesh me kqyr qet pjese qe e ke ba me ai me ba qysh e ka bo yt veq pa returns te res.
+router.get<{ userId: string }>(
+  "/getAllByUserID/:userId",
+  async (req: Request<{ userId: string }>, res: Response) => {
+    try {
+      const userId = req.params.userId;
+      const records = await FinancialRecordModel.find({ userId });
+
+      if (records.length === 0) {
+        res.status(404).send("No records found for the userin."); // No return
+        return; // Explicit return to exit function
+      }
+
+      res.status(200).send(records); // No return
+    } catch (err) {
+      const errorMessage =
+        err instanceof Error ? err.message : "Unknown errori";
+      res.status(500).send(errorMessage); // No return
+    }
+  }
+);
+
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    const newRecordBody = req.body;
+    const newRecord = new FinancialRecordModel(newRecordBody);
+    const savedRecord = await newRecord.save();
+
+    res.status(200).send(savedRecord);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+export default router;
