80bfd5b49ad868099f7393e7985656ba5c76dc64
alketagg <alketagashii13@gmail.com>
2025-05-24 15:42:28 +0200

    Shtimi linqeve

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Dashboard } from './pages/dashboard';
 import { Auth } from './pages/auth';
 import { FinancialRecordsProvider } from './contexts/financial-record-context';
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router>
       <div className="app-container">
+        <div className="navbar"></div>
         <Routes>
           <Route
             path="/"
