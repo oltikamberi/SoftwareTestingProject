c1b6d069b7a7c939d4ef6c19f0a3080e5c807b9d
alketagg <alketagashii13@gmail.com>
2025-05-28 15:22:25 +0200

    income forma

---
+++ client/src/pages/income/income-form.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { useUser } from '@clerk/clerk-react';
+import { useIncomeRecords } from '../../contexts/income-context';
+
+export const IncomeForm = () => {
+  const [source, setSource] = useState('');
+  const [amount, setAmount] = useState('');
+  const { addIncome } = useIncomeRecords();
+  const { user } = useUser();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newIncome = {
+      userId: user?.id ?? '',
+      date: new Date(),
+      source,
+      amount: parseFloat(amount),
+    };
+
+    addIncome(newIncome);
+    setSource('');
+    setAmount('');
+  };
+
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <label>Source</label>
+        <input
+          className="input"
+          value={source}
+          onChange={(e) => setSource(e.target.value)}
+          required
+        />
+        <label>Amount</label>
+        <input
+          className="input"
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          required
+        />
+        <button type="submit" className="button">
+          Add Income
+        </button>
+      </form>
+    </div>
+  );
+};
